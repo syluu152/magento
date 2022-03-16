fbfcec5209ec0eae05a22ecbabadf8572b84e16a
Sy Luu <syluu152@gmail.com>
2022-03-16 17:26:44 +0700

    init

---
+++ app/code/Magetop/Helloworld/Setup/InstallSchema.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Magetop\Helloworld\Setup;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+        $tableName = $installer->getTable('magetop_blog');
+        //Check for the existence of the table
+        if ($installer->getConnection()->isTableExists($tableName) != true) {
+            $table = $installer->getConnection()
+                ->newTable($tableName)
+                ->addColumn(
+                    'id',
+                    Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'ID'
+                )
+                ->addColumn(
+                    'title',
+                    Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false, 'default' => ''],
+                    'Title'
+                )
+                ->addColumn(
+                    'description',
+                    Table::TYPE_TEXT,
+                    null,
+                    ['nullable' => false, 'default' => ''],
+                    'Description'
+                )
+                ->addColumn(
+                    'created_at',
+                    Table::TYPE_DATETIME,
+                    null,
+                    ['nullable' => false],
+                    'Created At'
+                )
+                ->addColumn(
+                    'status',
+                    Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false, 'default' => '0'],
+                    'Status'
+                )
+                //Set comment for magetop_blog table
+                ->setComment('Magetop Blog Table')
+                //Set option for magetop_blog table
+                ->setOption('type', 'InnoDB')
+                ->setOption('charset', 'utf8');
+            $installer->getConnection()->createTable($table);
+        }
+        $installer->endSetup();
+    }
+}
---
+++ app/design/frontend/AHT/test/web/js/main.js
@@ -0,0 +1,18 @@
+// require([
+//     'jquery',
+//     'slick',
+//     'domReady!'
+//  ], function ($) {
+//     $(document).ready(function () {
+//         alert("Hello");
+//     });
+//  });
+
+// Navigation Tabs
+
+    // require([
+    //     'jquery',
+    //     'tabs'], function ($) {
+    //     $(".navigation-tab").tabs();
+    // });
+
